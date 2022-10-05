e78c4efebebc54bd3a5a94009c42e6849915fccc
Millenium-Falcon-NOT-Garbage <55559153+Millenium-Falcon-NOT-Garbage@users.noreply.github.com>
2022-10-05 09:01:00 -0400

    fixed robot container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,13 +11,10 @@ import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-<<<<<<< HEAD
 import edu.wpi.first.wpilibj.XboxController;
-=======
->>>>>>> 2f12d1fcdb46c219d66d5de2a270b5509555abb9
+import edu.wpi.first.wpilibj.XboxController.Button;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Constants.Intake;
-import frc.robot.Constants.Launcher;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.ChimneyCommand;
 import frc.robot.commands.IntakeCommand;
 import frc.robot.commands.JoystickDriveCommand;
@@ -51,26 +48,20 @@ public class RobotContainer {
     private final DoubleSupplier m_driveX = () -> m_driverMoveController.getX();
     private final DoubleSupplier m_driveY = () -> m_driverMoveController.getY();
     private final DoubleSupplier m_driveOmega = () -> m_driverTurnController.getX();
-    private final BooleanSupplier m_intakePower = () -> m_driverTurnController.getTrigger();
-    private final BooleanSupplier m_outtakePower = () -> m_driverMoveController.getTrigger();
 
     // private final BooleanSupplier m_fieldCentric = () ->
     // !(m_driverMoveController.getTrigger()
     // || m_driverTurnController.getTrigger());
     private final BooleanSupplier m_fieldCentric = () -> false;
-<<<<<<< HEAD
     // private final BooleanSupplier m_fieldCentric = () -> !(m_driverMoveController.getTrigger()
     //         || m_driverTurnController.getTrigger());
     // private final BooleanSupplier m_fieldCentric = () -> false;
-=======
->>>>>>> 2f12d1fcdb46c219d66d5de2a270b5509555abb9
 
     // The robot's subsystems and commands are defined here...
     private final DrivetrainSubsystem m_drivetrainSubsystem = new DrivetrainSubsystem(DataLogManager.getLog());
     private final CompressorSubsystem m_compressorSubsystem = new CompressorSubsystem();
-    private final IntakeSubsystem m_intakeSubsystem = new IntakeSubsystem(m_compressorSubsystem,
-            Constants.Intake.INTAKE_PORT,
-            Constants.Intake.RIGHT_SOLENOID_PORT, Constants.Intake.LEFT_SOLENOID_PORT);
+    private final IntakeSubsystem m_intakeSubsystem = new IntakeSubsystem(m_compressorSubsystem, Constants.Intake.INTAKE_PORT,
+                                                                          Constants.Intake.RIGHT_SOLENOID_PORT, Constants.Intake.LEFT_SOLENOID_PORT);
     private final ChimneySubsystem m_chimneySubsystem = new ChimneySubsystem();
     private final KickerSubsystem m_kickerSubsystem = new KickerSubsystem();
     private final LauncherSubsystem m_launcherSubsystem = new LauncherSubsystem();
@@ -78,16 +69,20 @@ public class RobotContainer {
 
     private final JoystickDriveCommand m_dDriveCommand = new JoystickDriveCommand(m_drivetrainSubsystem, m_driveX,
             m_driveY, m_driveOmega, m_fieldCentric);
-    private final IntakeCommand m_dIntakeCommand = new IntakeCommand(m_intakeSubsystem,
-            () -> m_intakePower.getAsBoolean() ? 0.8 : m_outtakePower.getAsBoolean() ? -0.8 : 0,
-            () -> m_intakePower.getAsBoolean() ? true : m_outtakePower.getAsBoolean() ? true : false);
-    private final ChimneyCommand m_dChimneyCommand = new ChimneyCommand(m_chimneySubsystem,
-            () -> m_intakePower.getAsBoolean() ? 0.8 : m_outtakePower.getAsBoolean() ? -0.8 : 0);
+
+    private final IntakeCommand m_dIntakeCommand = new IntakeCommand(m_intakeSubsystem, 
+    () -> m_driverTurnController.getTrigger() ? 0.8 : m_driverMoveController.getTrigger() ? -0.8 : 0, 
+    () -> m_driverTurnController.getTrigger() ? true : m_driverMoveController.getTrigger() ? true : false);
+
+    private final ChimneyCommand m_dChimneyCommand = new ChimneyCommand(m_chimneySubsystem, 
+    () -> m_driverTurnController.getTrigger() ? 0.8 : m_driverMoveController.getTrigger() ? -0.8 : 0);
+
     private final KickerCommand m_dKickerCommand = new KickerCommand(m_kickerSubsystem, () -> 0.0);
+
     private final LauncherCommand m_dLauncherCommand = new LauncherCommand(m_launcherSubsystem,
-            () -> SmartDashboard.getNumber("Set Launcher Velocity", 0), () -> false);
-    private final LimelightDriveCommand m_dLimelightDriveCommand = new LimelightDriveCommand(m_drivetrainSubsystem,
-            m_limelight, m_driveX, m_driveY, m_fieldCentric);
+     () -> SmartDashboard.getNumber("Set Launcher Velocity", 0), () -> false);
+
+    private final LimelightDriveCommand m_LimelightDriveCommand = new LimelightDriveCommand(m_drivetrainSubsystem, m_limelight, m_driveX, m_driveY, m_fieldCentric);
 
     /**
      * The container for the robot. Contains subsystems, OI devices, and commands.
@@ -100,7 +95,6 @@ public class RobotContainer {
         DataLogManager.start();
         DataLogManager.logNetworkTables(true);
         DriverStation.startDataLog(DataLogManager.getLog(), true);
-
     }
 
     private void configureButtonBindings() {
